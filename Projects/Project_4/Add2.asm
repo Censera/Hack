35d269e10f90f644c5523d347ef7798db5ab8667
Censera <Censera@proton.me>
2025-12-22 17:47:20 +0100

    Adds Up Two Numbers

--- Projects/Project_4/Add2.asm
+++ Projects/Project_4/Add2.asm
@@ -4,11 +4,13 @@
 // You need to change their value manually
 
 @0
-D=M // D = RAM[0]
+D=M			// D = RAM[0]
 
 @1
-D=D+M // RAM[1] = D + RAM[0]
+D=D+M		// RAM[1] = D + RAM[0]
 
 @2
-M=D // RAM[2] = D
+M=D			// RAM[2] = D
 
+@7			// END of the program
+0;JMP
