93768534cda2833f57ad6b563e9929aeb6ec444f
Censera <Censera@proton.me>
2025-12-22 18:14:24 +0100

    Is It Negative?

--- Projects/Project_4/Signum.asm
+++ Projects/Project_4/Signum.asm
@@ -2,21 +2,23 @@
 // If R0 > 0 then set R1 to 1
 //           else set R1 to 0
 
-@R0
-D=M         // D = RAM[0]
+    @R0
+    D=M         // D = RAM[0]
 
-@8
-D;JGT       // Jump if R0 > 0
+    @POSITIVE
+    D;JGT       // Jump if R0 > 0
 
-@R1
-M=0         // Set RAM[1] to 0 because the
-            // value of RAM[0] is negative
+    @R1
+    M=0         // Set RAM[1] to 0 because the
+                // value of RAM[0] is negative.
 
-@7         // END of the program
-0;JMP
+    @END        // Jump to the END of the program
+    0;JMP
 
-@R1
-M=1
+(POSITIVE)
+    @R1         // The value is positive,
+    M=1         // so we set RAM[1] to 1.
 
-@11        // END of the program
-0;JMP
+(END)           // END of the program
+    @END
+    0;JMP
